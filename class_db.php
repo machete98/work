f93d2a039026fc62a0fe5b36c562f4884e4c74c5
machete98 <48528915+machete98@users.noreply.github.com>
2019-03-13 20:46:56 +0300

    Create class_db.php

---
+++ class_db.php
@@ -0,0 +1,72 @@
+<?php
+class DataBase {
+
+  private static $db = null;
+  private $mysqli;
+  private $sym_query = "{?}"; // "Символ значения в запросе"
+
+  public static function getDB() {
+    if (self::$db == null) self::$db = new DataBase();
+    return self::$db;
+  }
+
+  private function __construct() {
+    $this->mysqli = new mysqli("localhost", "root", "", "my_db");
+    $this->mysqli->query("SET lc_time_names = 'ru_RU'");
+    $this->mysqli->query("SET NAMES 'utf8'");
+  }
+  
+  private function getQuery($query, $params) {
+    if ($params) {
+      for ($i = 0; $i < count($params); $i++) {
+        $pos = strpos($query, $this->sym_query);
+        $arg = "'".$this->mysqli->real_escape_string($params[$i])."'";
+        $query = substr_replace($query, $arg, $pos, strlen($this->sym_query));
+    }
+    }
+    return $query;
+  }
+
+  public function select($query, $params = false) {
+    $result_set = $this->mysqli->query($this->getQuery($query, $params));
+    if (!$result_set) return false;
+    return $this->resultSetToArray($result_set);
+  }
+
+  public function selectRow($query, $params = false) {
+    $result_set = $this->mysqli->query($this->getQuery($query, $params));
+    if ($result_set->num_rows != 1) return false;
+    else return $result_set->fetch_assoc();
+  }
+
+  public function selectCell($query, $params = false) {
+    $result_set = $this->mysqli->query($this->getQuery($query, $params));
+    if ((!$result_set) || ($result_set->num_rows != 1)) return false;
+    else {
+      $arr = array_values($result_set->fetch_assoc());
+      return $arr[0];
+    }
+  }
+
+  public function query($query, $params = false) {
+    $success = $this->mysqli->query($this->getQuery($query, $params));
+    if ($success) {
+      if ($this->mysqli->insert_id === 0) return true;
+      else return $this->mysqli->insert_id;
+    }
+    else return false;
+  }
+
+  private function resultSetToArray($result_set) {
+    $array = array();
+    while (($row = $result_set->fetch_assoc()) != false) {
+      $array[] = $row;
+    }
+    return $array;
+  }
+
+  public function __destruct() {
+    if ($this->mysqli) $this->mysqli->close();
+  }
+}
+?>
